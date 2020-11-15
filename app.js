aa1cf6213b7facf07a2c4e9612d70d0f1a08bd6d
Spencer Tassone <spencerwilliamtassone@gmail.com>
2020-11-15 18:32:46 -0500

    canvas rendering from socket response

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var serv = require('http').Server(app);
 
 app.get('/', function(req, res) {
-        res.sendFile(__dirname + '/client/index.html');
+    res.sendFile(__dirname + '/client/index.html');
 });
 
 app.use("client", express.static(__dirname + "/client"));
@@ -11,12 +11,27 @@ app.use("client", express.static(__dirname + "/client"));
 serv.listen(2000);
 console.log(" ===> Server has started... ")
 
+var SOCKET_LIST = {};
+
 var io = require("socket.io")(serv,{});
+io.sockets.on('connection', function(socket) { 
+    console.log(" ===> Socket is connected")
 
-io.sockets.on('connection', function(socket) {
-    console.log(" ===> New socket connection");
-
-    socket.on('happy', function(data) {
-        console.log("happy because " + data.reason)
-    })
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    SOCKET_LIST[socket.id] = socket;
+    
 });
+
+setInterval(function() {
+    for(var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i];
+        socket.x++;
+        socket.y++;
+        socket.emit('newPosition', {
+            x: socket.x,
+            y: socket.y
+        })
+    }
+}, 1000/25)
