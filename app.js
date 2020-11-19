bf155bd2afcb808cfd7d18c133f839a95217e20f
Spencer Tassone <spencerwilliamtassone@gmail.com>
2020-11-19 11:56:34 -0500

    remove console logs

--- app.js
+++ app.js
@@ -66,8 +66,6 @@ var Player = function(id) {
         else if(self.pressingDown) {
            self.spdY = self.maxSpd;
         } else self.spdY = 0
-        console.log("x: " + self.x)
-        console.log("y: " + self.y)
     }
     Player.list[id] = self;
 
@@ -125,7 +123,7 @@ Bullet.update = function() {
         var bullet = Bullet.list[i];
         bullet.update
     }
-}
+} 
 var io = require("socket.io")(serv,{});
 io.sockets.on('connection', function(socket) { 
     console.log(" ===> A socket is connected")
